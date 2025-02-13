7aa47130c5004975295520fb6432f1bc210f6e12
prvmjsky <lukyanovalexey1@gmail.com>
2025-02-13 14:36:47 +0500

    add picocli option and parameters for gendiff in App and remove greeting from main()

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,16 +2,26 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
 
-@Command(name = "gendiff",
-        mixinStandardHelpOptions = true,
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
 
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
-        System.out.println("Hello World!");
     }
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, defaultValue = "stylish", paramLabel = "format",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format;
 
     @Override
     public void run() {}
