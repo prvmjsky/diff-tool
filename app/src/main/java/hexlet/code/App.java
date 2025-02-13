b2fc6ce566ff99a2ee1edc6831da2cd49d96e93c
prvmjsky <lukyanovalexey1@gmail.com>
2025-02-13 13:51:50 +0500

    add picocli help info for gendiff in App and empty generate() in Differ

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,18 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+
     public static void main(String[] args) {
+        new CommandLine(new App()).execute(args);
         System.out.println("Hello World!");
     }
+
+    @Override
+    public void run() {}
 }
