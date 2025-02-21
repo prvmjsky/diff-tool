4ee31c0a39b13d8ce3295b73d6d313fb5a48c2bf
prvmjsky <lukyanovalexey1@gmail.com>
2025-02-21 21:43:56 +0500

    add yml scenario to Parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,8 +12,18 @@ import java.util.Map;
 public class Parser {
 
     public static Map<String, Object> parseToMap(String path) throws IOException {
+
+        ObjectMapper mapper;
+
+        if (path.endsWith(".json")) {
+            mapper = new ObjectMapper();
+        } else if (path.endsWith(".yml") || path.endsWith(".yaml")) {
+            mapper = new YAMLMapper();
+        } else {
+            throw new IOException("wrong file extension");
+        }
+
         File file = Paths.get(path).normalize().toAbsolutePath().toFile();
-        ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(file, new TypeReference<>() { });
     }
 }
