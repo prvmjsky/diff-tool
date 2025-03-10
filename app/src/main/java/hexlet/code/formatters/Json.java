5e9b48fef5e0e722d4607cbca183847494e0237d
prvmjsky <lukyanovalexey1@gmail.com>
2025-03-10 21:26:02 +0500

    rework Json formatter

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import hexlet.code.ComparableLine;
 import hexlet.code.Formatter;
 import hexlet.code.Parser;
-import hexlet.code.entities.JsonDiff;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -12,29 +11,38 @@ import java.util.LinkedHashMap;
 public final class Json extends Formatter {
     @Override
     public String formatDiff(ArrayList<ComparableLine> lines) throws JsonProcessingException {
-        var diff = new JsonDiff();
-        var before = new LinkedHashMap<String, Object>();
-        var after = new LinkedHashMap<String, Object>();
-
+        var diff = new LinkedHashMap<String, LinkedHashMap<String, Object>>();
         var sortedLines = lines.stream().sorted(ComparableLine::compareTo);
+
         sortedLines.forEach(line -> {
             var key = line.getKey();
             var value = line.getValue();
+            var oldValue = line.getOldValue();
+            var info = new LinkedHashMap<String, Object>();
 
             switch (line.getStatus()) {
-                case "same" -> diff.getSame().put(key, value);
-                case "added" -> diff.getAdded().put(key, value);
-                case "removed" -> diff.getRemoved().put(key, value);
+                case "same" -> {
+                    info.put("status", "same");
+                    info.put("value", value);
+                }
+                case "added" -> {
+                    info.put("status", "added");
+                    info.put("value", value);
+                }
+                case "removed" -> {
+                    info.put("status", "removed");
+                    info.put("value", value);
+                }
                 case "updated" -> {
-                    before.put(key, line.getOldValue());
-                    after.put(key, value);
+                    info.put("status", "updated");
+                    info.put("oldValue", oldValue);
+                    info.put("value", value);
                 }
                 default -> throw new IllegalArgumentException("Unknown status of line");
             }
-        });
 
-        diff.getUpdated().put("before", before);
-        diff.getUpdated().put("after", after);
+            diff.put(key, info);
+        });
 
         return Parser.toString(diff);
     }
