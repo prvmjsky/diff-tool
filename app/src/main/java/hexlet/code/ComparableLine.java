687edf67281861a6e5cd6dd15c8315aec142124f
prvmjsky <lukyanovalexey1@gmail.com>
2025-03-10 20:02:14 +0500

    rework Differ

--- app/src/main/java/hexlet/code/ComparableLine.java
+++ app/src/main/java/hexlet/code/ComparableLine.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 @Getter
@@ -26,6 +30,42 @@ public final class ComparableLine implements Comparable<ComparableLine> {
         var before = new ComparableLine("removed", this.key, this.oldValue);
         var after = new ComparableLine("added", this.key, this.value);
         return List.of(before, after);
+    }
+
+    public static ArrayList<ComparableLine> compareMaps(HashMap<String, Object> map1, HashMap<String, Object> map2) {
+        var lines = new ArrayList<ComparableLine>();
+
+        if (map1.equals(map2)) {
+            map1.forEach((key, value) -> {
+                lines.add(new ComparableLine("same", key, value));
+            });
+
+        } else {
+            var keysUnion = new HashSet<>(map1.keySet());
+            keysUnion.addAll(map2.keySet());
+
+            keysUnion.forEach(key -> {
+                var value1 = map1.get(key);
+                var value2 = map2.get(key);
+
+                if (map1.containsKey(key) && map2.containsKey(key)) {
+
+                    if (Objects.equals(value1, value2)) {
+                        lines.add(new ComparableLine("same", key, value1));
+                    } else {
+                        lines.add(new ComparableLine("updated", key, value2, value1));
+                    }
+
+                } else if (map1.containsKey(key)) {
+                    lines.add(new ComparableLine("removed", key, value1));
+
+                } else {
+                    lines.add(new ComparableLine("added", key, value2));
+                }
+            });
+        }
+
+        return lines;
     }
 
     @Override
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,10 +1,7 @@
 package hexlet.code;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Objects;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2) throws IOException {
@@ -12,39 +9,9 @@ public class Differ {
     }
 
     public static String generate(String filepath1, String filepath2, String format) throws IOException {
-        var lines = new ArrayList<ComparableLine>();
         var map1 = getMap(filepath1);
         var map2 = getMap(filepath2);
-
-        if (map1.equals(map2)) {
-            map1.forEach((key, value) -> {
-                lines.add(new ComparableLine("same", key, value));
-            });
-
-        } else {
-            var keysUnion = new HashSet<>(map1.keySet());
-            keysUnion.addAll(map2.keySet());
-
-            keysUnion.forEach(key -> {
-                var value1 = map1.get(key);
-                var value2 = map2.get(key);
-
-                if (map1.containsKey(key) && map2.containsKey(key)) {
-
-                    if (Objects.equals(value1, value2)) {
-                        lines.add(new ComparableLine("same", key, value1));
-                    } else {
-                        lines.add(new ComparableLine("updated", key, value2, value1));
-                    }
-
-                } else if (map1.containsKey(key)) {
-                    lines.add(new ComparableLine("removed", key, value1));
-
-                } else {
-                    lines.add(new ComparableLine("added", key, value2));
-                }
-            });
-        }
+        var lines = ComparableLine.compareMaps(map1, map2);
 
         var formatter = Formatter.getFormatter(format);
         return formatter.formatDiff(lines);
